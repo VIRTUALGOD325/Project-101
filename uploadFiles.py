e5f8cbfad3b4f7c7db98917ea927d1a8cf29a02b
VIRTUALGOD325 <65289865+VIRTUALGOD325@users.noreply.github.com>
2021-10-08 22:21:16 +0530

    Add files via upload

--- uploadFiles.py
+++ uploadFiles.py
@@ -1,25 +1,22 @@
-from os import *
 import dropbox
 
 class TransferData:
-    def __init__(self,access_token):
+    def __init__(self, access_token):
         self.access_token = access_token
 
-    def file_upload(self, file_from, file_to):
-        db = dropbox.Dropbox(self.access_token)
-
-        with open(file_from, 'rb') as f:
-            db.files_upload(f.read(), file_to)
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+        f = open(file_from, 'rb')
+        dbx.files_upload(f.read(), file_to)
 
 def main():
     access_token = 'XoCgXDHO3boAAAAAAAAAAVo4OrWeNxHZD18__qDcDWvM2jpN8lh3O_wvAgrwjT-m'
     transferData = TransferData(access_token)
+    file_from = input("Enter the file path to transfer : -")
+    file_to = input("enter the full path to upload to dropbox:- ")  
+    transferData.upload_file(file_from, file_to)
+    print("file has been moved !!!")
 
-    file_from = input('ENTER THE FILE/PATH TO BE UPLOADED ON DROPBOX')
-    file_to = input('GIVE THE LOCATION TO UPLOAD THE FILE')
-    file_to = "/test/"+file_to
 
-    transferData.file_upload(file_from,file_to)
-    print("THE FILE HAS BEEN UPLOADED SUCCESSFULLY")
+main()
 
-main()  
